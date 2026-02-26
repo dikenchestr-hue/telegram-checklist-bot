32fe7fd1592b8578c3b158b107f279e03231ecb0
dikenchestr-hue <diken_chestr@mail.ru>
2026-02-26 17:24:49 +0300

    Update __init__.py

--- questions/__init__.py
+++ questions/__init__.py
@@ -1,8 +1,48 @@
 # questions/__init__.py
-from .base import *
-from .review import *
-from .natural import *
-from .medical import *
-from .pedagog import *
-from .social import *
-from .humanities import *
+from .review import REVIEW_QUESTIONS
+from .natural import NATURAL_QUESTIONS
+from .medical import MEDICAL_QUESTIONS
+from .pedagog import PEDAGOGICAL_QUESTIONS
+from .social import SOCIAL_QUESTIONS
+from .humanities import HUMANITIES_QUESTIONS
+
+# Словарь для быстрого доступа ко всем вопросам
+QUESTIONS_MAP = {
+    'review': REVIEW_QUESTIONS,
+    'natural': NATURAL_QUESTIONS,
+    'medical': MEDICAL_QUESTIONS,
+    'pedagog': PEDAGOGICAL_QUESTIONS,
+    'social': SOCIAL_QUESTIONS,
+    'humanities': HUMANITIES_QUESTIONS
+}
+
+# Названия дисциплин
+DISCIPLINE_NAMES = {
+    'review': "ОБЗОРНАЯ СТАТЬЯ",
+    'natural': "ЕСТЕСТВЕННЫЕ И ТЕХНИЧЕСКИЕ НАУКИ",
+    'medical': "МЕДИЦИНСКИЕ НАУКИ",
+    'pedagog': "ПЕДАГОГИЧЕСКИЕ НАУКИ",
+    'social': "СОЦИАЛЬНЫЕ НАУКИ",
+    'humanities': "ГУМАНИТАРНЫЕ НАУКИ"
+}
+
+# Соответствие кнопок и кодов дисциплин
+DISCIPLINE_BUTTON_MAP = {
+    "📖 Гуманитарные науки": 'humanities',
+    "🔬 Естественные и технические науки": 'natural',
+    "🏥 Медицинские науки": 'medical',
+    "📚 Педагогические науки": 'pedagog',
+    "📊 Социальные науки": 'social'
+}
+
+__all__ = [
+    'QUESTIONS_MAP',
+    'DISCIPLINE_NAMES',
+    'DISCIPLINE_BUTTON_MAP',
+    'REVIEW_QUESTIONS',
+    'NATURAL_QUESTIONS',
+    'MEDICAL_QUESTIONS',
+    'PEDAGOGICAL_QUESTIONS',
+    'SOCIAL_QUESTIONS',
+    'HUMANITIES_QUESTIONS'
+]
