47ab8aa0aac406d6671c4f9db97207b2ce077af5
dikenchestr-hue <diken_chestr@mail.ru>
2026-02-26 16:56:58 +0300

    Create __init__.py

---
+++ questions/__init__.py
@@ -0,0 +1,8 @@
+# questions/__init__.py
+from .base import *
+from .review import *
+from .natural import *
+from .medical import *
+from .pedagog import *
+from .social import *
+from .humanities import *
